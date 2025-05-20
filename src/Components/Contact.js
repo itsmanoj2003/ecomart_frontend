a3eb078cf314cb75cba6681ac597a0d26658ea5e
itsmanoj2003 <manojprabhakaran524@gmail.com>
2025-05-20 21:11:47 +0530

    Updated contact page design and dependencies

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,47 +1,39 @@
-import React from 'react'
-import './Contact.css'
+import React from 'react';
+import './Contact.css';
+import { FaWhatsapp, FaInstagram, FaEnvelope } from 'react-icons/fa';
 
-
-import Whatsapp from '../Components/assets/whatsapp.png'
-import Instagram from '../Components/assets/instagram.png'
-import Gmail from '../Components/assets/gmail.png'
-import Phone from '../Components/assets/phone.png'
-
-export default function Contact() {
+const Contact = () => {
   return (
-    <div className='contact'>
+    <div className="contact-wrapper">
+      <div className="contact-card">
+        <div className="contact-left">
+          <h2>Ecomart</h2>
+          <p className='addr'>Thiruvenkadam - Sankarnkovil Main Road<br />
+             Sankarankovil, Tenkasi District</p>
+          <p><strong>Email:</strong> <a href="mailto:ecomartsangai@gmail.com" className='cont-details'>ecomartsangai@gmail.com</a></p>
+          <p><strong>WhatsApp:</strong> <a href="https://wa.me/917200260036" target="_blank" rel="noopener noreferrer" className='cont-details'>+91 7200260036</a></p>
+          <p><strong>Instagram:</strong> <a href="https://instagram.com/ecomart_bazaar" target="_blank" rel="noopener noreferrer" className='cont-details'>@ecomart_bazaar</a></p>
 
-        <div className='contact-main'>
-
-            <div className='contactimg-container1'>
-                    <img src={Whatsapp} className='whatsapp-img'/>
-                    <h1 className='contact-title'>EcoMart</h1>
-
-                    <label className='address-title'>Address</label>
-                    <div className='address-container'>
-                        <p className='mart-address'>Thiruvenkadam - Sankarnkovil Main Road <br/> Sankarankovil,Tenkasi District</p>
-                    </div>
-                    
-                    <img src={Instagram} className='instagram-img'/>
-                    <img src={Gmail} className='gmail-img'/>
-            </div>
-
-                <fieldset className='contact-content'>
-                <legend className='contact-legent'>Reach us on</legend>
-
-                <div className='contact-details'>
-                <label className='socialmedia-names'>Whatsapp</label> <a href='tel:+916374358764'><h2 className='whatsappnumber'>+91 7200260036</h2></a> <br></br>
-                <label className='socialmedia-names'>Instagram</label> <a href='https://www.instagram.com/accounts/login/?hl=en'><h2 className='instagramid'>@ecomart_bazaar</h2></a> <br></br>
-                <label className='socialmedia-names'>Gmail</label> <a href='mailto:manojprabhakaran524@gmail.com'><h2 className='gmailaccount'>ecomartsangai@gmail.com</h2></a> <br></br>
-                </div>
-
-                </fieldset>
-
-            <div className='contactimg-container2'>
-                <img src={Phone} className='phone-img'/>
-            </div>
+          <div className="contact-socials">
+            <a href="https://wa.me/917200260036" target="_blank" rel="noopener noreferrer">
+              <FaWhatsapp className="social-icon whatsapp" />
+            </a>
+            <a href="https://instagram.com/ecomart_bazaar" target="_blank" rel="noopener noreferrer">
+              <FaInstagram className="social-icon instagram" />
+            </a>
+            <a href="mailto:ecomartsangai@gmail.com">
+              <FaEnvelope className="social-icon email" />
+            </a>
+          </div>
         </div>
 
+        <div className="contact-right">
+          <h3>We’d love to hear from you!</h3>
+          <p>Feel free to reach out for any queries, suggestions or partnership opportunities.</p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
+
+export default Contact;
