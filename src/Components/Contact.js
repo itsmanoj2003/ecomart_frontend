bb0fa55e38b9a539fcdc64a87b1009636da9edb8
itsmanoj2003 <manojprabhakaran524@gmail.com>
2025-07-29 20:07:26 +0530

    Updated Contact.js

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -16,13 +16,13 @@ const Contact = () => {
              Sankarankovil, Tenkasi District</p>
           <p><strong>Email:</strong> <a href="mailto:ecomartsangai@gmail.com" className='cont-details'>ecomartsangai@gmail.com</a></p>
           <p><strong>WhatsApp:</strong> <a href="https://wa.me/917200260036" target="_blank" rel="noopener noreferrer" className='cont-details'>+91 7200260036</a></p>
-          <p><strong>Instagram:</strong> <a href="https://instagram.com/ecomart_bazaar" target="_blank" rel="noopener noreferrer" className='cont-details'>@ecomart_bazaar</a></p>
+          <p><strong>Instagram:</strong> <a href="https://www.instagram.com/ecomart_sangai?utm_source=ig_web_button_share_sheet&igsh=MW03MzliMWtnNDYxMg==" target="_blank" rel="noopener noreferrer" className='cont-details'>@ecomart_sangai</a></p>
 
           <div className="contact-socials">
             <a href="https://wa.me/917200260036" target="_blank" rel="noopener noreferrer">
               <FaWhatsapp className="social-icon whatsapp" />
             </a>
-            <a href="https://instagram.com/ecomart_bazaar" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.instagram.com/ecomart_sangai?utm_source=ig_web_button_share_sheet&igsh=MW03MzliMWtnNDYxMg==" target="_blank" rel="noopener noreferrer">
               <FaInstagram className="social-icon instagram" />
             </a>
             <a href="mailto:ecomartsangai@gmail.com">
@@ -49,9 +49,9 @@ const Contact = () => {
             <h2 style={{fontSize:'25px'}}>Soft Stor Technology</h2>
 
             <p style={{fontSize:'13px',marginRight:'190px',marginTop:'20px'}}>Mobile No:</p>
-            <h5 style={{fontSize:'13px',marginRight:'155px',marginTop:'10px'}}>+91 6374358764</h5>
-            <h5 style={{fontSize:'13px',marginRight:'155px',marginTop:'10px'}}>+91 8610729420</h5>
-            <h5 style={{fontSize:'13px',marginRight:'155px',marginTop:'10px'}}>+91 7358916638</h5>
+            <h5 style={{fontSize:'13px',marginRight:'180px',marginTop:'10px'}}><a href="tel:+916374358764" target="_blank" rel="noopener noreferrer" style={{color:'white'}}>+91 6374358764</a></h5>
+            <h5 style={{fontSize:'13px',marginRight:'180px',marginTop:'10px'}}><a href="tel:+918610729420" target="_blank" rel="noopener noreferrer" style={{color:'white'}}>+91 8610729420</a></h5>
+            <h5 style={{fontSize:'13px',marginRight:'180px',marginTop:'10px'}}><a href="tel:+917358916638" target="_blank" rel="noopener noreferrer" style={{color:'white'}}>+91 7358916638</a></h5>
 
             <div className="contact-socials">
             <a href="https://softstortechnology.netlify.app/" target="_blank" rel="noopener noreferrer">
